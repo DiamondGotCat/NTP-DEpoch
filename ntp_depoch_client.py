8d3479eca95d2f5eb6cd13b08cfe3f373da7f8a6
DiamondGotCat <chii.2019.5555@gmail.com>
2025-03-25 00:54:08 +0900

    Create ntp_depoch_client.py

---
+++ ntp_depoch_client.py
@@ -0,0 +1,25 @@
+import socket
+from ntp_depoch_common import now_dgc_epoch_ms, pack_packet, unpack_packet
+
+SERVER = "127.0.0.1"
+PORT = 12345
+BUF_SIZE = 28
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(2)
+
+t1 = now_dgc_epoch_ms()
+req = pack_packet(1, 3, t1, 0, 0)
+sock.sendto(req, (SERVER, PORT))
+
+data, _ = sock.recvfrom(BUF_SIZE)
+t4 = now_dgc_epoch_ms()
+
+_, _, orig, t2, t3 = unpack_packet(data)
+
+offset = ((t2 - t1) + (t3 - t4)) / 2
+delay = (t4 - t1) - (t3 - t2)
+
+print(f"T₁={t1}, T₂={t2}, T₃={t3}, T₄={t4}")
+print(f"Offset: {offset:.3f} ms, Delay: {delay:.3f} ms")
+print(f"Corrected time (DGC Epoch): {now_dgc_epoch_ms() + offset:.0f} ms")
